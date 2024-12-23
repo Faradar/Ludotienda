3322e7c502bfe0bf232101886ade4d5a78424dbb
Faradar <tomi_compuamste@hotmail.com>
2024-12-23 19:38:46 -0300

    clase2

--- App.js
+++ App.js
@@ -3,7 +3,14 @@ import { Text, View, StyleSheet } from "react-native";
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text> Hello World </Text>
+      <View style={styles.contenedorPartes}>
+        <View style={styles.parteUno}>
+          <Text style={styles.text}>Parte 1</Text>
+        </View>
+        <View style={styles.parteDos}>
+          <Text style={styles.text}>Parte 2</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -12,8 +19,35 @@ export default App;
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "red",
+    width: "100%",
     flex: 1,
-    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
+    color: "white",
+  },
+  contenedorPartes: {
+    backgroundColor: "yellow",
+    width: "80%",
+    marginHorizontal: "10%",
+  },
+  parteUno: {
+    backgroundColor: "blue",
+    width: "60%",
+    padding: 20,
+    margin: 10,
+    flex: 3, // 3/4 of it's parent containers height
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  parteDos: {
+    backgroundColor: "green",
+    width: "80%",
+    padding: 30,
+    margin: 10,
+    flex: 1, // 1/4 of it's parent containers height
     alignItems: "center",
     justifyContent: "center",
   },
